9a75f8be59c968fa2955b369085c8c65c0eb1b5e
Stephen Foster <stephen@thoughtstem.com>
2023-07-02 08:56:41 -0700

    gh pages config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const repo = 'social-games'
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+const nextConfig = {
+  output: 'export',
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+}
 
 module.exports = nextConfig
